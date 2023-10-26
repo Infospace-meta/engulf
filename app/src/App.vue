f8b3d8f3dd8da80070bbe77dea3ce26f033e8bcc
joe052 <joekaranjasenior52@gmail.com>
2023-10-26 18:21:03 +0300

    message

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,9 +1,17 @@
 <template>
-	<RouterView />
+  <div class="min-h-screen bg-white">
+    <Header />
+    <div class="mt-4">
+      <RouterView />
+    </div>
+    <Footer />
+  </div>
 </template>
 
 <script setup>
 import { RouterView } from "vue-router";
+import Header from "./components/header.vue"
+import Footer from "./components/footer.vue"
 </script>
 
 <style scoped></style>
