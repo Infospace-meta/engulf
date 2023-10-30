65512e16b3a132493116d89d87a2c9e4b5766c40
immanuel kimani <immanuel4082@gmail.com>
2023-10-30 15:23:08 +0300

    message

---
+++ app/src/components/home-Page/about.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div>
+      <div>
+        <h1>About Us</h1>
+        <img src="" alt="" />
+      </div>
+      <div>
+        <p></p>
+      </div>
+    </div>
+    <div>
+      <div>
+        <p>Guiding Vision</p>
+      </div>
+      <div>
+        <h1></h1>
+        <img src="" alt="" />
+      </div>
+    </div>
+    <div>
+      <div>
+        <h1>Mission statement</h1>
+        <img src="" alt="" />
+      </div>
+      <div>
+        <p></p>
+      </div>
+    </div>
+  </div>
+</template>
--- app/src/components/home-Page/hero.vue
+++ app/src/components/home-Page/hero.vue
@@ -1,16 +1,36 @@
 <template>
-  <div>
-    <div>
-      <h1>It's time to elevate your<span>financial future</span></h1>
-      <p>
+  <div
+    class="flex flex-col-reverse lg:flex-row justify-center lg:justify-between lg:px-96 px-10 items-center h-screen"
+  >
+    <div class="">
+      <h1 class="text-5xl font-semibold">
+        It's time to elevate <br />your<span class="text-red-500">
+          financial future</span
+        >
+      </h1>
+      <p class="py-5 text-lg">
         Discover a world of financial opportunities with Pro Forex Investments.
       </p>
-      <div>
-        <router-link :to="{ name: 'home' }">Contact</router-link>
-        <!-- <router-link>Enroll with Us</router-link> -->
+      <div class="flex gap-5">
+        <router-link
+          :to="{ name: 'home' }"
+          class="bg-slate-400 text-white p-2 rounded-md"
+          >Enroll with Us</router-link
+        >
+        <router-link
+          :to="{ name: 'home' }"
+          class="bg-red-500 text-white p-2 rounded-md"
+          >Contact</router-link
+        >
       </div>
     </div>
-    <div><img src="src/assets/images/heroImage.jpg" alt="heroImage" /></div>
+    <div>
+      <img
+        src="src/assets/images/heroImage.jpg"
+        alt="heroImage"
+        class="h-76 lg:h-96"
+      />
+    </div>
   </div>
 </template>
 
