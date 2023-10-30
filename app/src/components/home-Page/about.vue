145eb7d99d48220d972e188f5702f3527e2471c6
immanuel kimani <immanuel4082@gmail.com>
2023-10-30 16:51:14 +0300

    message

--- app/src/components/home-Page/about.vue
+++ app/src/components/home-Page/about.vue
@@ -1,30 +1,90 @@
 <template>
-  <div>
-    <div>
-      <div>
-        <h1>About Us</h1>
-        <img src="" alt="" />
+  <div class="px-1 lg:px-96">
+    <div class="grid lg:grid-cols-2 p-10">
+      <div class="flex flex-col gap-4 justify-center items-center">
+        <h1 class="text-3xl">
+          About <br /><span class="text-4xl text-red-500 font-semibold"
+            >Us</span
+          >
+        </h1>
+        <img
+          src="src/assets/images/info.png"
+          alt=""
+          class="w-20 hover:animate-pulse"
+        />
       </div>
       <div>
-        <p></p>
+        <p class="text-lg lg:text-center">
+          XYZ Forex is a dynamic and forward-thinking forex trading company that
+          was founded with a passion for empowering individuals and businesses
+          to harness the opportunities presented by the foreign exchange market.
+          With years of experience and a team of dedicated professionals, we
+          have established ourselves as a trusted partner for those seeking to
+          diversify their investment portfolio and explore the world of currency
+          trading. <br />As a customer-centric organization, we pride ourselves
+          on providing a secure, user-friendly trading platform, backed by
+          cutting-edge technology, to facilitate seamless and efficient forex
+          trading experiences. We understand that each client is unique, and we
+          offer a range of account types, educational resources, and
+          personalized support to cater to diverse needs and skill levels.
+        </p>
       </div>
     </div>
-    <div>
+    <div class="grid lg:grid-cols-2 p-10">
       <div>
-        <p>Guiding Vision</p>
+        <p class="text-lg text-center">
+          At XYZ Forex, our guiding vision is to become a globally recognized
+          leader in the foreign exchange industry. We aim to empower individuals
+          and businesses with the knowledge and tools they need to navigate the
+          complex world of currency trading successfully. We believe that by
+          delivering exceptional services, staying at the forefront of
+          technological advancements, and fostering a culture of continuous
+          learning, we can achieve our vision of becoming the go-to destination
+          for forex trading
+        </p>
       </div>
-      <div>
-        <h1></h1>
-        <img src="" alt="" />
+      <div class="flex flex-col gap-4 justify-center items-center">
+        <h1 class="text-3xl">
+          Guiding <br /><span class="text-4xl text-red-500 font-semibold"
+            >Vision</span
+          >
+        </h1>
+        <img
+          src="src/assets/images/about.png"
+          alt=""
+          class="w-20 hover:animate-ping"
+        />
       </div>
     </div>
-    <div>
-      <div>
-        <h1>Mission statement</h1>
-        <img src="" alt="" />
+    <div class="grid lg:grid-cols-2 p-10">
+      <div class="flex flex-col gap-4 justify-center items-center">
+        <h1 class="text-3xl">
+          Mission <br /><span class="text-4xl text-red-500 font-semibold"
+            >statement</span
+          >
+        </h1>
+        <img
+          src="src/assets/images/mission.png"
+          alt=""
+          class="w-20 hover:animate-bounce"
+        />
       </div>
       <div>
-        <p></p>
+        <p class="text-lg text-center">
+          Our mission at XYZ Forex is to provide accessible and innovative forex
+          trading solutions that enable our clients to achieve financial
+          success. We are committed to delivering superior customer service,
+          offering a wide range of trading instruments, and maintaining
+          transparency and integrity in all our operations. We strive to educate
+          and empower our clients, whether they are seasoned traders or
+          newcomers to the forex market. We believe that by equipping our
+          clients with the knowledge, tools, and support they need to make
+          informed trading decisions, we can help them unlock the vast potential
+          of the foreign exchange market. As a company, we are dedicated to
+          fostering trust, transparency, and long-term relationships with our
+          clients, ultimately realizing our vision of becoming a global leader
+          in the forex industry.
+        </p>
       </div>
     </div>
   </div>
