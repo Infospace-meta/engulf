81ff087d53330f6f536535979808a5e5dd27417a
joe052 <joekaranjasenior52@gmail.com>
2023-11-08 12:13:00 +0300

    message

--- app/src/views/ShowView.vue
+++ app/src/views/ShowView.vue
@@ -33,7 +33,7 @@
   <!-- <StrategiesModal /> -->
 </template>
 <script setup>
-import Subscription from "../components/home-Page/Subscription.vue";
+import Subscription from "../components/home-Page/subscription.vue";
 import About from "../components/home-Page/about.vue";
 import ChooseUs from "../components/home-Page/chooseUs.vue";
 import ContactUs from "../components/home-Page/contactsUs.vue";
