6dfdcba9b4f0097ce478243ca4e260143f23f90a
immanuel kimani <immanuel4082@gmail.com>
2023-11-21 16:30:08 +0300

    message

--- app/src/components/home-Page/hero.vue
+++ app/src/components/home-Page/hero.vue
@@ -1,6 +1,11 @@
 <template>
+  <meta
+    http-equiv="Content-Security-Policy"
+    content="script-src 'self' 'unsafe-inline' 'unsafe-eval' https://s3.tradingview.com;"
+  />
+
   <div
-    class="flex flex-col-reverse lg:flex-row lg:justify-center lg:items-center h-screen pattern-plus-red-50/100"
+    class="flex max-md:justify-center max-md:items-center max-md:text-center lg:flex-row lg:justify-center lg:items-center h-screen pattern-plus-red-50/100"
   >
     <div class="">
       <h1 class="text-5xl font-semibold">
@@ -24,14 +29,105 @@
         >
       </div>
     </div>
-    <div>
-      <img
+    <div class="flex justify-center items-center">
+      <!-- <img
         src="src/assets/images/heroImage.jpg"
         alt="heroImage"
         class="h-76 lg:h-96"
-      />
+      /> -->
+      <div
+        class="tradingview-widget-container max-md:hidden"
+        ref="widgetContainer"
+      ></div>
     </div>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+// Import necessary Vue features
+import { ref, onMounted } from "vue";
+
+// Create a reference to the widget container
+const widgetContainer = ref(null);
+
+onMounted(() => {
+  const script = document.createElement("script");
+  script.type = "text/javascript";
+  script.src =
+    "https://s3.tradingview.com/external-embedding/embed-widget-market-overview.js";
+  script.async = true;
+
+  // Simplified widget configurations for illustration
+  const widgetConfig = {
+    colorTheme: "light",
+    dateRange: "1D",
+    showChart: true,
+    locale: "en",
+    largeChartUrl: "",
+    isTransparent: true,
+    showSymbolLogo: true,
+    showFloatingTooltip: false,
+    width: "500",
+    height: "500",
+    plotLineColorGrowing: "rgba(106, 168, 79, 1)",
+    plotLineColorFalling: "rgba(255, 0, 0, 1)",
+    gridLineColor: "rgba(240, 243, 250, 0)",
+    scaleFontColor: "rgba(106, 109, 120, 1)",
+    belowLineFillColorGrowing: "rgba(106, 168, 79, 0.12)",
+    belowLineFillColorFalling: "rgba(255, 0, 0, 0.12)",
+    belowLineFillColorGrowingBottom: "rgba(41, 98, 255, 0)",
+    belowLineFillColorFallingBottom: "rgba(255, 0, 0, 0)",
+    symbolActiveColor: "rgba(182, 182, 182, 0.12)",
+    tabs: [
+      {
+        title: "Indices",
+        symbols: [
+          { s: "OANDA:XAUUSD", d: "GOLD" },
+          { s: "FX:EURUSD", d: "US/EURO" },
+          { s: "FX:USDJPY", d: "US DOLLAR / YEN" },
+        ],
+        originalTitle: "Indices",
+      },
+      {
+        title: "Futures",
+        symbols: [
+          { s: "CME_MINI:ES1!", d: "S&P 500" },
+          { s: "CME:6E1!", d: "Euro" },
+          { s: "COMEX:GC1!", d: "Gold" },
+          { s: "NYMEX:CL1!", d: "WTI Crude Oil" },
+          { s: "NYMEX:NG1!", d: "Gas" },
+          { s: "CBOT:ZC1!", d: "Corn" },
+        ],
+        originalTitle: "Futures",
+      },
+      {
+        title: "Bonds",
+        symbols: [
+          { s: "CBOT:ZB1!", d: "T-Bond" },
+          { s: "CBOT:UB1!", d: "Ultra T-Bond" },
+          { s: "EUREX:FGBL1!", d: "Euro Bund" },
+          { s: "EUREX:FBTP1!", d: "Euro BTP" },
+          { s: "EUREX:FGBM1!", d: "Euro BOBL" },
+        ],
+        originalTitle: "Bonds",
+      },
+      {
+        title: "Forex",
+        symbols: [
+          { s: "FX:EURUSD", d: "EUR to USD" },
+          { s: "FX:GBPUSD", d: "GBP to USD" },
+          { s: "FX:USDJPY", d: "USD to JPY" },
+          { s: "FX:USDCHF", d: "USD to CHF" },
+          { s: "FX:AUDUSD", d: "AUD to USD" },
+          { s: "FX:USDCAD", d: "USD to CAD" },
+        ],
+        originalTitle: "Forex",
+      },
+    ],
+  };
+
+  script.text = `new TradingView.MarketWidget(${JSON.stringify(widgetConfig)})`;
+
+  widgetContainer.value.appendChild(script);
+});
+</script>
--- app/src/components/home-Page/strategies/strategies.vue
+++ app/src/components/home-Page/strategies/strategies.vue
@@ -1,6 +1,9 @@
-<!-- <template>
+<template>
   <div class="flex justify-center items-center">
-    <div class="tradingview-widget-container  max-w-7xl" style="height: 500px; width: 100%">
+    <div
+      class="tradingview-widget-container max-w-7xl"
+      style="height: 500px; width: 100%"
+    >
       <div :id="widgetId" style="height: calc(100% - 32px); width: 100%"></div>
       <div class="tradingview-widget-copyright">
         <a
@@ -65,78 +68,5 @@ export default {
       widgetId,
     };
   },
-};
-</script> -->
-<template>
-  <div
-    class="tradingview-widget-container max-w-7xl"
-    style="height: 100px; width: 100%"
-  >
-    <div :id="widgetId" class="tradingview-widget-container__widget"></div>
-    <div class="tradingview-widget-copyright">
-      <a
-        href="https://www.tradingview.com/"
-        rel="noopener nofollow"
-        target="_blank"
-      >
-        <span class="blue-text">Track all markets on TradingView</span>
-      </a>
-    </div>
-  </div>
-</template>
-
-<script setup>
-import { ref, onMounted } from "vue";
-
-const widgetId = ref("tradingview-ticker-tape");
-const widgetScriptSrc = ref(
-  "https://s3.tradingview.com/external-embedding/embed-widget-ticker-tape.js"
-);
-
-const createWidget = () => {
-  if (window.TradingView) {
-    new window.TradingView.widget({
-      symbols: [
-        { proName: "FOREXCOM:SPXUSD", title: "S&P 500" },
-        { proName: "FOREXCOM:NSXUSD", title: "US 100" },
-        { proName: "FX_IDC:EURUSD", title: "EUR to USD" },
-        { proName: "BITSTAMP:BTCUSD", title: "Bitcoin" },
-        { proName: "BITSTAMP:ETHUSD", title: "Ethereum" },
-        { description: "Gold commodity", proName: "OANDA:XAUUSD" },
-      ],
-      showSymbolLogo: true,
-      colorTheme: "light",
-      isTransparent: true,
-      displayMode: "adaptive",
-      locale: "en",
-      container_id: widgetId.value,
-    });
-  } else {
-    console.error("TradingView object is not available.");
-  }
-};
-
-const onLoadScript = () => {
-  createWidget();
-};
-
-onMounted(() => {
-  loadTradingViewScript().then(onLoadScript);
-});
-
-const loadTradingViewScript = () => {
-  if (!window.TradingView) {
-    return new Promise((resolve) => {
-      const script = document.createElement("script");
-      script.id = "tradingview-widget-ticker-tape-script";
-      script.src = widgetScriptSrc.value;
-      script.type = "text/javascript";
-      script.onload = resolve;
-
-      document.head.appendChild(script);
-    });
-  } else {
-    return Promise.resolve();
-  }
 };
 </script>
